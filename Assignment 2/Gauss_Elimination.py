fb85800af62e801d1c61bdedc574f6865a0d58b5
Sasila Hapuarachchi <sasilaah@gmail.com>
2022-02-07 17:18:55 -0500

    Added functions and helper for question 4

--- Assignment 2/Gauss_Elimination.py
+++ Assignment 2/Gauss_Elimination.py
@@ -1,5 +1,6 @@
 import numpy
-from numpy import array, zeros, random
+from numpy import array, zeros
+from matplotlib import pyplot as plt
 
 
 def Gauss_Elimination(A_mat, b_vec):
@@ -87,6 +88,8 @@ def swap_rows(mat, vec, row1, row2):
     vec[[row1, row2]] = mat[[row2, row1]]
 
 # With pivoting
+
+
 def Forward_Elimination_Pivoting(A_mat, b_vec):
     # number of linear equations
     n = len(b_vec)
@@ -115,30 +118,38 @@ def Forward_Elimination_Pivoting(A_mat, b_vec):
 
     return A_mat, b_vec
 
-
 # Generate random test case with an n x n coefficient matrix and an n x 1 right-hand side vector filled with random values from -100 to 100
+
+
 def random_test_case(n):
-    A_mat = array(random.uniform(-100.0, 100.0, (n,n)), float)
-    b_vec = array(random.uniform(-100.0, 100.0, n), float)
+    A_mat = array(numpy.random.uniform(-100.0, 100.0, (n, n)), float)
+    b_vec = array(numpy.random.uniform(-100.0, 100.0, n), float)
     return A_mat, b_vec
 
 # Generate random test cases with an n x n coefficient matrix and an n x 1 right-hand side vector filled with random values specified in range
+
+
 def random_test_case_in_range(n, low, high):
-    A_mat = array(random.uniform(low, high, (n,n)), float)
-    b_vec = array(random.uniform(low, high, n), float)
+    A_mat = array(numpy.random.uniform(low, high, (n, n)), float)
+    b_vec = array(numpy.random.uniform(low, high, n), float)
     return A_mat, b_vec
 
-# Checks solution by multiplying coefficient matrix with solution vector and comparing with constant vector    
+# Checks solution by multiplying coefficient matrix with solution vector and comparing with constant vector
+
+
 def validate_solution(A_mat, b_vec, solu_vec):
     calculated_constant_vector = numpy.dot(A_mat, solu_vec)
-    print("-> Verfication: The calculated b_vec is:" + str(calculated_constant_vector))
+    print("-> Verfication: The calculated b_vec is:" +
+          str(calculated_constant_vector))
     print("-> Verification: The actual b_vec is:" + str(b_vec))
     return numpy.allclose(calculated_constant_vector, b_vec)
 
-# Creates an nxn example matrix and confirms solution by multiplying 
+# Creates an nxn example matrix and confirms solution by multiplying
 # A with X to compare with b vector for both pivoting and non-pivoting solutions.
+
+
 def part2_q2_examples(n):
-    print("\n|||Examples for ",n,"x",n," matrix|||" )
+    print("\n|||Examples for ", n, "x", n, " matrix|||")
     A_nxn, b_nxn = random_test_case(n)
 
     print("Initial A_mat is: \n", A_nxn)
@@ -148,7 +159,8 @@ def part2_q2_examples(n):
     solu_vec_nxn_pivoting = Gauss_Elimination_Pivoting(A_nxn, b_nxn)
     solu_vec_numpy = numpy.linalg.solve(A_nxn, b_nxn)
     print("The Gauss Elimination solution is: " + str(solu_vec_nxn))
-    print("The Gauss Elimination solution with Pivoting is: " + str(solu_vec_nxn_pivoting))
+    print("The Gauss Elimination solution with Pivoting is: " +
+          str(solu_vec_nxn_pivoting))
     print("The Numpy calculated solution is: " + str(solu_vec_numpy))
 
     # Confirm solutions by multiplying coefficient matrix with solution vector and comparing with constant vector
@@ -156,7 +168,67 @@ def part2_q2_examples(n):
     print("* Gauss Elimination Verification: " + str(confirm_nxn))
 
     confirm_nxn_pivoting = validate_solution(A_nxn, b_nxn, solu_vec_nxn)
-    print("* Gauss Elimination with Pivoting Verification: " + str(confirm_nxn_pivoting))
+    print("* Gauss Elimination with Pivoting Verification: " +
+          str(confirm_nxn_pivoting))
+
+# Function to perform Q4 steps for a given value of k
+def part2_q4_error_calculations(k):
+    solution_vecs = []
+
+    # Perform Q4 Steps 100 times
+    for i in range(100):
+
+        # Generate random 10x10 test case and corresponding solution
+        A_10x10, b_10x10 = random_test_case(10)
+        solu_vec_10x10 = Gauss_Elimination(A_10x10, b_10x10)
+
+        # Perturb coefficient matrix and constant elements by adding random values between -10^-k, 10**(-k)
+        # and find corresponding solution using new A_coefficients and b_elements
+        A_10x10_Perturbed = A_10x10 + \
+            array(numpy.random.uniform(-1 * 10**(-k),
+                  1 * 10**(-k), (10, 10)), float)
+        b_10x10_Perturbed = b_10x10 + \
+            array(numpy.random.uniform(-1 * 10**(-k), 1 * 10**(-k), 10), float)
+        solu_vec_10x10_Perturbed = Gauss_Elimination(
+            A_10x10_Perturbed, b_10x10_Perturbed)
+
+        # Calculate error using E = sqrt(delta_x1^2 + delta_x2^2 + ..... delta_x10^2 )
+        sum_of_squares = numpy.sum(numpy.square(solu_vec_10x10 - solu_vec_10x10_Perturbed))
+        error = numpy.sqrt(sum_of_squares)
+        solution_vecs.append(error)
+
+    # Calculate mean and standard deviation of errors    
+    mean_of_error = numpy.mean(solution_vecs, axis=0)
+    standard_deviation_of_error = numpy.std(solution_vecs, axis=0)
+    return mean_of_error, standard_deviation_of_error
+
+def part2_q4_helper():
+    # [-10^-k, 10^-k]
+    list_of_means = []
+    list_of_standard_deviations = []
+
+    # Perform Q4 steps for k values from 0 to 10 and store the means and standard deviations
+    # in lists for later use
+    for k in range(10):
+        mean, standard_deviation = part2_q4_error_calculations(k)
+        list_of_means.append(mean)
+        list_of_standard_deviations.append(standard_deviation)
+
+    # Plot Mean of errors vs k graph
+    k_x = numpy.arange(0, 10)
+    plt.title("Mean of Errors vs k")
+    plt.xlabel("k")
+    plt.ylabel("Mean")
+    plt.plot(k_x, list_of_means)
+    plt.show()
+
+    # Plot Standard Deviation of errors vs k graph
+    plt.title("Standard Deviation of Errors vs k")
+    plt.xlabel("k")
+    plt.ylabel("Standard Deviation")
+    plt.plot(k_x, list_of_standard_deviations)
+    plt.show()
+
 
 def main():
     A_mat = array([[3, -2, 5, 0],
@@ -171,6 +243,8 @@ def main():
     # part2_q2_examples(2)
     # part2_q2_examples(3)
     # part2_q2_examples(4)
+
+    #part2_q4_helper()
 
 
 if __name__ == '__main__':
