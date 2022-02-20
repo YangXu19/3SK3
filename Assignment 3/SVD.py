9183b707d51651f8203028092e7a93c022609b94
Yang Xu <yangxu011900@gmail.com>
2022-02-20 18:55:07 -0500

    put code in functions

--- Assignment 3/SVD.py
+++ Assignment 3/SVD.py
@@ -2,29 +2,36 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.pyplot import imread
 
-img = imread('McMasterLogo.png')
+def main():
+    img = imread('McMasterLogo.png')
+    gray_img = grayscale(img)
+    ranks = [1,2,6,16,32,64,128,220]
+    svd_ranks(gray_img, ranks)
 
-rgb_weights = [0.2989, 0.5879, 0.1140]
+def grayscale(img):
+    rgb_weights = [0.2989, 0.5879, 0.1140]
+    gray_img = np.dot(img[...,:3], rgb_weights)
+    return gray_img
+    # plot gray image
+    # plt.imshow(gray_img, cmap=plt.get_cmap("gray"))
 
-gray_img = np.dot(img[...,:3], rgb_weights)
-# plt.imshow(gray_img, cmap=plt.get_cmap("gray"))
+def svd_ranks(gray_img, ranks):
+    U, S, VT = np.linalg.svd(gray_img, full_matrices=False)
+    S = np.diag(S)
 
-# SVD
-U, S, VT = np.linalg.svd(gray_img, full_matrices=False)
-S = np.diag(S)
+    j = 0
 
-j = 0
-for r in (1, 2, 4, 16, 32, 64, 128, 220):
-    Xapprox = U[:,:r] @ S[0:r,:r] @ VT[:r,:]
-    # plt.figure(j+1)
-    plt.subplot(5,5,j+1)
-    j += 1
-    img = plt.imshow(Xapprox)
-    img.set_cmap('gray')
-    plt.axis('off')
-    plt.title('r = ' + str(r))
+    for r in ranks:
+        Xapprox = U[:,:r] @ S[0:r,:r] @ VT[:r,:]
+        # plt.figure(j+1)
+        plt.subplot(5,5,j+1)
+        j += 1
+        img = plt.imshow(Xapprox)
+        img.set_cmap('gray')
+        plt.axis('off')
+        plt.title('r = ' + str(r))
 
-plt.show()
+    plt.show()
 
 # plt.ylabel('Singular Values')
 # plt.title('McMasterLogo.png Singular Values')
@@ -32,3 +39,6 @@ plt.show()
 # plt.plot(S)
 # print(S)
 # plt.show()
+
+if __name__ == '__main__':
+    main()
